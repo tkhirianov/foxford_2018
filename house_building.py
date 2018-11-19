d3fcb47a27708d5e5a0e6e201ae5d397e923f00e
Timofey Khirianov <tkhirianov@gmail.com>
2018-11-19 17:54:47 +0300

    Описал интерфейс функции рисования домика. Функция - заглушка.

--- house_building.py
+++ house_building.py
@@ -4,4 +4,16 @@ win_width = 600
 win_height = 500
 
 def main():
-    pass  # Ничего не делаем
+    draw_house(win_width//3, win_height*2//3, 150, 200)
+
+
+def draw_house(x0, y0, width, height):
+    """
+        Функция рисует дом в положении x0, y0 на холсте.
+        x0, y0 - центральная нижняя точка домика
+        width, height - ширина и высота.
+    """
+    pass # FIXME: сейчас эта функция на самом деле ничего не делает.
+    print("Заглушка!")
+
+main()
