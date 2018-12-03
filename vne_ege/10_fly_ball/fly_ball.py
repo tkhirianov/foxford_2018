7c0b7db584bfcf41f48092378df7b050934b64b3
Timofey Khirianov <tkhirianov@gmail.com>
2018-12-03 18:10:18 +0300

    Нуль-версия летающего шарика, хранящегося как список его параметров.
    Шарик ровно один и хранится глобально: single_ball
    Это плохо и требует исправления. Смотри #2

--- vne_ege/10_fly_ball/fly_ball.py
+++ vne_ege/10_fly_ball/fly_ball.py
@@ -2,50 +2,77 @@ import tkinter
 import time
 from random import randint
 
-oval_id = None
-x, y, r = 10, 20, 10
-dx, dy = 4, 2
+# Режим игры - игра идёт или нет
+game_began = False
+sleep_time = 50  # ms
 scores = 0
+
+# single_ball = [x, y, dx, dy, r, oval_id]
+single_ball = [10, 20, 5, 3, 1, None]
 
 
 def tick():
-    global x, y, dx, dy
-    time_label.after(200, tick)
+    time_label.after(sleep_time, tick)
     time_label['text'] = time.strftime('%H:%M:%S')
+    if game_began:
+        ball_step(single_ball)
+
+
+def button_start_game_handler():
+    global game_began
+    if not game_began:
+        start_game()
+        game_began = True
+
+
+def button_stop_game_handler():
+    global game_began
+    if game_began:
+        stop_game()
+        game_began = False
+
+
+def start_game():
+    global single_ball
+    ball_create(single_ball)
+
+
+def stop_game():
+    ball_delete(single_ball)
+
+
+def ball_create(ball):
+    x, y, dx, dy, r, oval_id = ball
+    if oval_id is None:
+        r = randint(10, 30)
+        x = randint(0 + r, 639 - r)
+        y = randint(0 + r, 479 - r)
+        oval_id = canvas.create_oval(x - r, y - r, x + r, y + r, fill='green')
+    ball[:] = x, y, dx, dy, r, oval_id
+
+
+def ball_delete(ball):
+    x, y, dx, dy, r, oval_id = single_ball
+    canvas.delete(oval_id)
+    oval_id = None
+    ball[:] = x, y, dx, dy, r, oval_id
+
+
+def ball_step(ball):
+    """
+    Сдвигает шарик ball в соответствии с его скоростью.
+    :param ball: список [x, y, dx, dy, r, oval_id]
+    """
+    x, y, dx, dy, r, oval_id = ball
     if oval_id is not None:
         x += dx
         y += dy
         if x + r >= 639 or x - r <= 0:
             dx = -dx
+        if y + r >= 479 or y - r <= 0:
+            dy = -dy
         canvas.coords(oval_id, (x - r, y - r, x + r, y + r))
-
-
-def start_game():
-    global oval_id
-    if oval_id is None:
-        oval_id = canvas.create_oval(x-r, y-r, x+r, y+r, fill='green')
-    else:
-        print("ПРЕДУПРЕЖДЕНИЕ: Игра ещё не началась!")
-
-
-def delete_ball():
-    global oval_id
-    canvas.delete(oval_id)
-    oval_id = None
-
-
-def click_handler(event):
-    global x, y, r, scores_text, scores
-    print(event.x, event.y)
-    if oval_id is not None:
-        if ((event.x - x)**2 + (event.y - y)**2) <= r**2:
-            print("Попал!")
-            scores += 100
-            scores_text["text"] = "Ваши очки: " + str(scores)
-            r = randint(10, 30)
-            x = randint(0+r, 639-r)
-            y = randint(0+r, 479-r)
-            canvas.coords(oval_id, (x-r, y-r, x+r, y+r))
+    ball[:] = x, y, dx, dy, r, oval_id
 
 
 root = tkinter.Tk("Лопни шарик!")
@@ -53,20 +80,18 @@ root.geometry("640x480")
 
 buttons_panel = tkinter.Frame(bg="gray", width=640)
 buttons_panel.pack(side=tkinter.TOP, anchor="nw", fill=tkinter.X)
-button_start = tkinter.Button(buttons_panel, text="Start", command=start_game)
+button_start = tkinter.Button(buttons_panel, text="Start",
+                              command=button_start_game_handler)
 button_start.pack(side=tkinter.LEFT)
-button_stop = tkinter.Button(buttons_panel, text="Stop", command=delete_ball)
+button_stop = tkinter.Button(buttons_panel, text="Stop",
+                             command=button_stop_game_handler)
 button_stop.pack(side=tkinter.LEFT)
-time_label = tkinter.Label(buttons_panel, font='sans 20')
+time_label = tkinter.Label(buttons_panel, font='sans 14')
 time_label.pack(side=tkinter.LEFT)
 scores_text = tkinter.Label(buttons_panel, text="Ваши очки: 0")
 scores_text.pack(side=tkinter.RIGHT)
-
 canvas = tkinter.Canvas(root, bg='lightgray')
 canvas.pack(anchor="nw", fill=tkinter.BOTH, expand=1)
-canvas.bind("<Button>", click_handler)
 
 time_label.after_idle(tick)
-
-
 root.mainloop()
