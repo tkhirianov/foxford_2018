b30c01a01ee9a274bd626934f04ff85ded1747a2
Timofey Khirianov <tkhirianov@gmail.com>
2018-12-24 17:58:18 +0300

    Улучшил заготовку игры.
    Декларировал методы классов и кратко описал их задачи.
    Нужно написать их содержание.

--- vne_ege/13_arithmetical/dragons.py
+++ vne_ege/13_arithmetical/dragons.py
@@ -7,14 +7,24 @@ class BattleUnit:
     """
     Отвечает за здоровье и атаку.
     """
-    pass
+    def __init__(self, health, attack_force):
+        self._health = health
+        self._attack_force = attack_force
+
+    def get_health(self):
+        return self._health
+
+    def cause_damage(self, other):
+        """ Причинить урон другой боевой единице."""
+        pass
 
 
 class Hero(BattleUnit):
     """
     Должен отвечать на заданный вопрос.
     """
-    pass
+    def get_answer(self, question: str) -> str:
+        pass
 
 
 class Dragon(BattleUnit):
@@ -23,25 +33,44 @@ class Dragon(BattleUnit):
       1) загадывать загадку и запоминать ответ,
       2) проверять корректность ответов.
     """
-    pass
+    def get_question(self):
+        """ Генерирует новый вопрос и запоминает ответ."""
+        pass
+
+    def check_answer(self, answer: str) -> bool:
+        """ Проверяет ответ на корректность. """
+        pass
 
 
 class Battle:
     """
     Проводит бой между игроком и драконом до полной победы одного из них.
     """
-    pass
+    def __init__(self, hero, dragon):
+        pass
+
+    def fight(self):
+        """ Собственно, запускает битву."""
+        pass
+
+    def hero_won(self) -> bool:
+        """ Правда, если бой выиграл герой."""
 
 
 class GameRound:
-    """
-    Контролирует один игровой раунд:
-      1) Генерирует список драконов (игровой уровень).
-      2) Приветствует новый раунд.
-      3) Последовательно запускает бои между героем и очередным драконом.
-      4) Отображает результаты игрового раунда.
-    """
-    pass
+    """ Контролирует один игровой раунд. """
+
+    def __init__(self, hero):
+        """ Генерирует список драконов (игровой уровень). """
+        pass
+
+    def play(self):
+        """ Запускает игровой раунд
+          1) Приветствует новый раунд.
+          2) Последовательно запускает бои между героем и очередным драконом.
+          3) Отображает результаты игрового раунда.
+        """
+        pass
 
 
 main()
